0c0c9fb08ba34e907d6ef9d4bec58f505651cd13
ayushrai1235 <prathibharai64@gmail.com>
2025-03-22 02:09:40 +0530

    implement user signup and login functionality; add password hashing and validation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,20 @@
+import User from "../models/user.models";
+
 export const signup = async(req, res) => {
-    res.send("Signup route");
+    const {name, email, password} = req.body;
+    const userExists = await User.findOne({email})
+    if(userExists){
+        res.status(400).json({
+            message: "User already exists"
+        })
+    }
+    const user = new User.create({
+        name,
+        email,
+        password
+    })
+
+    res.status(201).json({user, message: "User created successfully"})
 }
 
 export const login = async(req, res) => {
