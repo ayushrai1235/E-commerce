493763d8501d74013dcc5f56a8cb5f46a40428d2
ayushrai1235 <prathibharai64@gmail.com>
2025-03-31 16:10:34 +0530

    add product routes and controller; implement endpoint to retrieve all products

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth",authRoutes)
-
+app.use("/api/products", )
 app.listen(PORT, ()=> {
     console.log("Server is running on port http://localhost:"+ PORT);
 
